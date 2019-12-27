f8e432682d99d13c76843fdfb158959f2f3f8252
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2019-12-27 20:15:05 +0100

    Add Vendor lookup

---
+++ macvendor.rb
@@ -0,0 +1,16 @@
+class MacVendor
+  def self.initialize
+    @@vendorhash = {}
+    File.open("./oui.txt") do |f|
+      f.each do |line|
+        match = line.match /^([0-9A-F]{2})-([0-9A-F]{2})-([0-9A-F]{2})   \(hex\)\t\t(.*)\r$/
+        next unless match
+        key = match[1].downcase + ":" + match[2].downcase + ":" + match[3].downcase
+        @@vendorhash[key] = match[4]
+      end
+    end
+  end
+  def self.by_oid(oid)
+    @@vendorhash[oid]
+  end
+end
