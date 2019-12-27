e555da88a7a38f3f8b4258b062413d442bca8075
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2019-12-27 19:34:37 +0100

    Update supported group set, signature algorithms

--- tlsclienthello.rb
+++ tlsclienthello.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 class TLSClientHelloError < StandardError
 end
 
@@ -97,6 +99,18 @@ class TLSClientHello
     to_ret[:cipherdata][:des] = cdata.des_present?
     to_ret[:cipherdata][:humanreadable] = cdata.humanreadable
     to_ret[:cipherdata][:cipherset] = cdata.cipherset
+
+    to_ret[:cipherdata][:supported_group_set] = to_ret[:supportedgroups].join(" ")
+    to_ret[:cipherdata][:signature_algorithm_set] to_ret[:signaturealgorithms].join(" ")
+
+    to_ret[:fingerprinting][:v1] = Digest::SHA2.hexdigest(
+      to_ret[:version] + "|" +
+      to_ret[:cipherdata][:cipherset] + "|" +
+      to_ret[:cipherdata][:supported_group_set] + "|" +
+      to_ret[:cipherdata][:signature_algorithm_set] + "|" +
+      (to_ret[:cipherdata][:statusrequest] == [] ? "False" : to_ret[:cipherdata][:statusrequest]) + "|" +
+      (to_ret[:renegotiation] ? "True" : "False") + "|" +
+      (to_ret[:extendedmastersecret] ? "True" : "False") )
     to_ret
   end
 
--- update_elastic.rb
+++ update_elastic.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'elasticsearch'
+require 'digest'
 require './tlsciphersuites.rb'
 
 client = Elasticsearch::Client.new log: false
@@ -9,7 +10,7 @@ client = Elasticsearch::Client.new log: false
 loop do
 
 #data = client.search index: 'tlshandshakes', body: { size: 50, query: { bool: { must_not: { exists: { field: "tlsserverhello.cipherdata.auth" } } } } }
-data = client.search index: 'tlshandshakes', body: { size: 50, query: { bool: { must_not: { exists: { field: "tlsclienthello.cipherdata.humanreadable" } } } } }
+#data = client.search index: 'tlshandshakes', body: { size: 50, query: { bool: { must_not: { exists: { field: "tlsclienthello.cipherdata.humanreadable" } } } } }
 
 puts data["hits"]["hits"].length
 
@@ -18,11 +19,22 @@ break if data["hits"]["hits"].length == 0
 data["hits"]["hits"].each do |hit|
   body = hit["_source"]
 
-  cipher = TLSCipherSuite.new(body["tlsclienthello"]["ciphersuites"])
+  body["tlsclienthello"]["cipherdata"]["supported_group_set"] = body["tlsclienthello"]["supportedgroups"].join(" ")
+  body["tlsclienthello"]["cipherdata"]["signature_algorithm_set"] = body["tlsclienthello"]["signaturealgorithms"].join(" ")
+  body["tlsclienthello"]["fingerprinting"]["v1"] = Digest::SHA2.hexdigest(
+    body["tlsclienthellp"]["version"] + "|" +
+    body["tlsclienthello"]["cipherdata"]["cipherset"] + "|" +
+    body["tlsclienthello"]["cipherdata"]["supported_group_set"] + "|" +
+    body["tlsclienthello"]["cipherdata"]["signature_algorithm_set"] + "|"
+    (body["tlsclienthello"]["statusrequest"] == [] ? "False" : body["tlsclienthello"]["statusrequest"]) + "|" +
+    (body["tlsclienthello"]["renegotiation"] ? "True" : "False") + "|"
+    (body["tlsclienthello"]["extendedmastersecret"] ? "True" : "False") )
 
-  body["tlsclienthello"]["cipherdata"] ||= {}
-  body["tlsclienthello"]["cipherdata"]["humanreadable"] = cipher.humanreadable
-  body["tlsclienthello"]["cipherdata"]["cipherset"]  = cipher.cipherset
+  #cipher = TLSCipherSuite.new(body["tlsclienthello"]["ciphersuites"])
+
+  #body["tlsclienthello"]["cipherdata"] ||= {}
+  #body["tlsclienthello"]["cipherdata"]["humanreadable"] = cipher.humanreadable
+  #body["tlsclienthello"]["cipherdata"]["cipherset"]  = cipher.cipherset
   #body["tlsclienthello"]["cipherdata"]["pfs_avail"] = cipher.pfs_avail?
   #body["tlsclienthello"]["cipherdata"]["only_pfs"]  = cipher.only_pfs?
   #body["tlsclienthello"]["cipherdata"]["anull"]     = cipher.anull_present?
