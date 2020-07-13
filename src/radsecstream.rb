1cb679cd90f9b0c41d6e90ecf3196179079cedcc
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2020-07-13 20:19:54 +0200

    Add errors and housekeeping

--- src/radsecstream.rb
+++ src/radsecstream.rb
@@ -41,8 +41,8 @@ class RadsecStream
 
   def add_request(pkt)
     logger.trace("Add Request packet")
-    raise PacketFlowInsertionError unless @last_from_server
-    raise PacketFlowInsertionError if pkt.state != @current_state
+    raise PacketFlowInsertionError.new("Attempted to insert a request without previous answer from server") unless @last_from_server
+    raise PacketFlowInsertionError.new("Attempted to insert a request with a non matching state") if pkt.state != @current_state
 
     @last_from_server = false
     @current_pktid = pkt.identifier
@@ -52,8 +52,8 @@ class RadsecStream
   end
   def add_response(pkt)
     logger.trace("Add response packet")
-    raise PacketFlowInsertionError if @last_from_server
-    raise PacketFlowInsertionError if pkt.identifier != @current_pktid
+    raise PacketFlowInsertionError.new("Attempted to insert a response without previous request from client") if @last_from_server
+    raise PacketFlowInsertionError.new("Attempted to insert a response with not matching identifier") if pkt.identifier != @current_pktid
 
     @last_from_server = true
     @current_state = pkt.state
@@ -79,11 +79,26 @@ class RadsecStreamHelper
 
   @housekeeping_counter
 
+  # Initialize the known stream and set timeout
+  # @param timeout [Integer] number of seconds after a Radsec Stream is considered timed out. Defaults to 60
   def initialize(timeout=60)
     logger.trace("Initialize RadsecStreamHelper with timeout of #{timeout} seconds")
     @known_streams = []
     @timeout = timeout
     @housekeeping_counter = 0
+  end
+
+  # Private helper to add packet in Packetflow.
+  # This function is used internally and should never be called directly.
+  # @private
+  def priv_add_packet(pkt, request, src, dst)
+    if request
+      insert_request(pkt, src, dst)
+    else
+      insert_response(pkt, src, dst)
+    end
+    # After we inserted the packet we can do housecleaning.
+    housekeeping
   end
 
   def insert_request(pkt, client, server)
@@ -130,26 +145,42 @@ class RadsecStreamHelper
     flow.add_response(pkt)
   end
 
-  def priv_add_packet(pkt, request, src, dst)
-    if request
-      insert_request(pkt, src, dst)
-    else
-      insert_response(pkt, src, dst)
+  # Tidy up all timed out states
+  def housekeeping
+    @housekeeping_counter += 1
+    return if @housekeeping_counter < 10
+    logger.trace("Starting Housekeeping")
+    t = Time.now
+    old = @known_streams.select{ |x| (t-x.last_updated) > @timeout }
+    old.each do |o|
+      logger.info "Timing out 0x#{o.current_state.pack('C*').unpack('H*').first}" if o.current_state
+      logger.info 'Timing out state without state variable' unless o.current_state
+      @known_streams.delete o
     end
+    @housekeeping_counter = 0
   end
+  private :housekeeping
 
+  # Add Packet to Packetflow
+  # @param pkt [RadsecPacket] Packet to insert in the PacketFlow
   def self.add_packet(pkt, request, src, dst)
     logger.trace("Add packet to a stream")
     RadsecStreamHelper.instance.priv_add_packet(pkt, request, src, dst)
   end
 
+  # Start Parsing of a certain Packetflow once it is done.
+  # @param pktflow [RadsecStream] Stream to parse
   def self.notify_flow_done(pktflow)
     logger.trace("Notify Packetflow is done")
     RadsecStreamHelper.instance.priv_notify_flow_done(pktflow)
   end
 
+  # Private helper method
+  # @param pktflow [RadsecStream]
+  # @private
   def priv_notify_flow_done(pktflow)
     @known_streams.delete(pktflow)
     StackParser.insert_into_parser(:radsec, pktflow)
   end
+
 end
