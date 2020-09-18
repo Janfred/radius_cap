a28e055f5bfe378fa63aa8a2409f27759bfd829a
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2020-09-18 19:43:34 +0200

    Fix typo in fingerprint-DB, update update_fingerprint.rb

--- update_fingerprint.rb
+++ update_fingerprint.rb
@@ -12,35 +12,38 @@ fp_db = Fingerprint.get_fp_db
 
 fp_db.each do |fp, fp_data|
   puts "Update all fingerprints #{fp}"
-  loop do
-    data = client.search index: 'tlshandshakes', body: {
-        size: 150,
-        query: {
-            bool: {
-                filter: [
-                    {
-                        match_phrase: {
-                            "tls.tlsclienthello.fingerprinting.v2.keyword": fp
-                        }
-                    },
-                    {
-                        match_phrase: {
-                            "tls.tlsclienthello.fingerprinting.osdetails.os.keyword": "Not in FP-DB"
-                        }
-                    }
-                ]
-            }
-        }
-    }
+  [:os, :os_version, :detail].each do |field|
+    puts " Updating Field #{field.to_s}"
+    loop do
+      data = client.search index: 'tlshandshakes', body: {
+          size: 150,
+          query: {
+              bool: {
+                  filter: [
+                      {
+                          match_phrase: {
+                              "tls.tlsclienthello.fingerprinting.v2.keyword": fp
+                          }
+                      },
+                  ],
+                  must_not: {
+                      match_phrase: {
+                          "tls.tlsclienthello.fingerprinting.osdetails.#{field.to_s}.keyword": fp_data[field]
+                      }
+                  }
+              }
+          }
+      }
 
-    length = data["hits"]["hits"].length
-    break if length==0
-    puts "  #{length}"
+      length = data["hits"]["hits"].length
+      break if length==0
+      puts "  #{length}"
 
-    data["hits"]["hits"].each do |hit|
-      body = hit["_source"]
-      body["tls"]["tlsclienthello"]["fingerprinting"]["osdetails"] = Fingerprint.to_h fp
-      client.index index: hit["_index"], type: hit["_type"], id: hit["_id"], body: body
+      data["hits"]["hits"].each do |hit|
+        body = hit["_source"]
+        body["tls"]["tlsclienthello"]["fingerprinting"]["osdetails"] = Fingerprint.to_h fp
+        client.index index: hit["_index"], type: hit["_type"], id: hit["_id"], body: body
+      end
     end
   end
 end
